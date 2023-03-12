ad3ea043ef92da666f7e47c99237912522819840
Ansh Nagwekar <62483645+anshnagwekar@users.noreply.github.com>
2023-03-12 19:08:41 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      buttonText: "click me!"
     };
   }
 
   //handler goes here
+  handleClick = () => {
+    this.setState(state => ({
+      buttonText: state.buttonText === 'click me!' ? 'thanks' : 'click me!',
+    }));
+  };
 
   render() {
     return (
       <div>
         <p>Greetings!</p>
-        <p>Button goes here</p>
+        <button onClick={this.handleClick}>{this.state.buttonText}</button>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,23 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 
 // import your story from App.stories.js
-
+import { Default } from './App.stories';
 
 // Add your tests here
 // See https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests#example-with-testing-library
+test('button at default state upon rendering App', () => {
+    render(<Default {...Default.args} />);
+    const linkElement = screen.getByText(/click me!/i);
+    expect(linkElement).toBeInTheDocument();
+    expect(screen.getByRole('button').textContent).toBe('click me!');
+    expect(screen.getByRole('button').textContent).not.toBe('thanks');
+    });
+
+test('button changes text upon rendering App and clicking', () => {
+    render(<Default {...Default.args} />);
+    const linkElement = screen.getByText(/click me!/i);
+    fireEvent.click(linkElement);
+    expect(screen.getByText(/thanks/i)).toBeInTheDocument();
+    expect(screen.getByRole('button').textContent).not.toBe('click me!');
+    expect(screen.getByRole('button').textContent).toBe('thanks');
+    });
